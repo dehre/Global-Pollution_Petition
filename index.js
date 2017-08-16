8fcaffa91fc4f842d7704dedadccc9958971d9f4
Loris <loris4526@gmail.com>
2017-08-16 16:14:06 +0200

    Set up basic express server

---
+++ index.js
@@ -0,0 +1,32 @@
+//require modules
+const express = require('express');
+const hb = require('express-handlebars');
+
+//create express application
+const app = express();
+
+//set up templating engine
+app.engine('handlebars', hb());
+app.set('view engine', 'handlebars');
+
+//MIDDLEWARES
+app.use(require('body-parser').urlencoded({
+    extended: false
+}));
+app.use(require('cookie-parser')());
+//serve static files
+app.use(express.static(__dirname + '/static'));
+//CODE ROUTING
+app.get('/petition',function(req,res){
+  res.send('Petition page!')
+});
+app.post('/petition',function(req,res){
+  console.log('Body of inputs is',req.body);
+  res.redirect('/petition')
+});
+
+//start listening on port 8080
+const port = 8080;
+app.listen(port,function(){
+  console.log(`Server listening on port ${port}`);
+});
