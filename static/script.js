3e781b0a8bdfaa23a0dd3274bfa1060e369d0448
Loris <loris4526@gmail.com>
2017-08-16 15:59:57 +0200

    Fix bug on setting <input> for canvas

--- static/script.js
+++ static/script.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
 
   //take DOM references
-  const $canvas = $('#signature-canvas')
-  const context = document.getElementById('signature-canvas').getContext('2d');
+  const canvas = document.getElementById('signature-canvas');
+  const context = canvas.getContext('2d');
+  const $canvas = $('#signature-canvas');
+  const $canvasInput = $('input[name="signature"]');
+
+  //helper variables for drawing on <canvas>
+  let emptyCanvas = true;
   let mouseOnCanvas = false;
   let lastX, lastY;
 
-  function draw(x, y, isDown) {
+  //draw line if mouse is down, otherwise just save current mouse position
+  function draw(x, y, isDown){
     if(isDown){
       context.beginPath();
       context.moveTo(lastX, lastY);
@@ -17,22 +23,32 @@ $(document).ready(function(){
     lastX = x; lastY = y;
   }
 
+  //translate signature into 'base-64' text and  set it as value of hidden <input name="signature">
+  function grabCanvasImage(){
+    if(!emptyCanvas){
+      console.log(canvas.toDataURL());
+      $canvasInput.val(canvas.toDataURL());
+    }
+  }
+
+  //track mouse movements to allow user drawing signature
   $canvas.mousedown(function(event){
+    emptyCanvas = false;
     mouseOnCanvas = true;
     draw(event.offsetX, event.offsetY, false);
   });
-  $canvas.mousemove(function (e) {
+  $canvas.mousemove(function(event){
     if(mouseOnCanvas){
       draw(event.offsetX, event.offsetY, true);
     }
   });
-  $canvas.mouseup(function (e) {
+  $canvas.mouseup(function(){
     mouseOnCanvas = false;
+    grabCanvasImage();
   });
-  $canvas.mouseleave(function (e) {
+  $canvas.mouseleave(function(){
     mouseOnCanvas = false;
+    grabCanvasImage();
   });
-
-
 
 }); //end $(document).ready()
