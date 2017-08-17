5112b117e8422061b28efa5e84244071b954fad0
Loris <loris4526@gmail.com>
2017-08-17 07:38:50 +0200

    Separate routes into their own file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hb = require('express-handlebars');
 const middlewares = require('./middlewares');
-const dbMethods = require('./db/methods');
+const routes = require('./routes');
 
 //create express application
 const app = express();
@@ -13,39 +13,8 @@ app.set('view engine', 'handlebars');
 //set up middlewares
 middlewares(app);
 
-// // ROUTING
-app.get('/petition',function(req,res){
-  res.render('petition',{
-    showError: false
-  });
-});
-
-app.post('/petition',function(req,res){
-  const {firstName,lastName,signature} = req.body;
-  //need all <input> fields to be filled
-  if(firstName && lastName && signature){
-    //save signed person to database
-    dbMethods.savePerson(firstName,lastName,signature)
-    .then(function(results){
-      //set a cookie to remember signed-in user
-      res.cookie('signed','true');
-      //redirect user away
-      res.redirect('/signed');
-    })
-    .catch(function(err){
-      res.send('Error happened saving data to DB');
-    });
-  } else {
-    //if not all fields were filled, just render the petition page again with an error message
-    res.render('petition',{
-      showError: true
-    });
-  }
-});
-
-app.get('/signed',function(req,res){
-  res.send('Signed page here!')
-});
+// set up routes
+routes(app);
 
 //start listening on port 8080
 const port = 8080;
---
+++ routes.js
@@ -0,0 +1,38 @@
+const dbMethods = require('./db/methods');
+//set all routes used inside express app
+module.exports = function(app){
+
+  app.get('/petition',function(req,res){
+    res.render('petition',{
+      showError: false
+    });
+  });
+
+  app.post('/petition',function(req,res){
+    const {firstName,lastName,signature} = req.body;
+    //need all <input> fields to be filled
+    if(firstName && lastName && signature){
+      //save signed person to database
+      dbMethods.savePerson(firstName,lastName,signature)
+      .then(function(results){
+        //set a cookie to remember signed-in user
+        res.cookie('signed','true');
+        //redirect user away
+        res.redirect('/signed');
+      })
+      .catch(function(err){
+        res.send('Error happened saving data to DB');
+      });
+    } else {
+      //if not all fields were filled, just render the petition page again with an error message
+      res.render('petition',{
+        showError: true
+      });
+    }
+  });
+
+  app.get('/signed',function(req,res){
+    res.send('Signed page here!')
+  });
+
+};
