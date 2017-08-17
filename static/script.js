aead392533888bdc09f579687ff58553adac6e4c
Loris <loris4526@gmail.com>
2017-08-17 08:18:01 +0200

    Fix frontend js script to check if <canvas> in current page before running code

--- static/script.js
+++ static/script.js
@@ -1,53 +1,57 @@
 $(document).ready(function(){
 
-  //take DOM references
+  //check if canvas in current webpage
   const canvas = document.getElementById('signature-canvas');
-  const context = canvas.getContext('2d');
-  const $canvas = $('#signature-canvas');
-  const $canvasInput = $('input[name="signature"]');
+  if(canvas){
+    //take other DOM references
+    const context = canvas.getContext('2d');
+    const $canvas = $('#signature-canvas');
+    const $canvasInput = $('input[name="signature"]');
 
-  //helper variables for drawing on <canvas>
-  let emptyCanvas = true;
-  let mouseOnCanvas = false;
-  let lastX, lastY;
+    //helper variables for drawing on <canvas>
+    let emptyCanvas = true;
+    let mouseOnCanvas = false;
+    let lastX, lastY;
 
-  //draw line if mouse is down, otherwise just save current mouse position
-  function draw(x, y, isDown){
-    if(isDown){
-      context.beginPath();
-      context.moveTo(lastX, lastY);
-      context.lineTo(x, y);
-      context.closePath();
-      context.stroke();
+    //draw line if mouse is down, otherwise just save current mouse position
+    function draw(x, y, isDown){
+      if(isDown){
+        context.beginPath();
+        context.moveTo(lastX, lastY);
+        context.lineTo(x, y);
+        context.closePath();
+        context.stroke();
+      }
+      lastX = x; lastY = y;
     }
-    lastX = x; lastY = y;
-  }
 
-  //translate signature into 'base-64' text and  set it as value of hidden <input name="signature">
-  function grabCanvasImage(){
-    if(!emptyCanvas){
-      $canvasInput.val(canvas.toDataURL());
+    //translate signature into 'base-64' text and  set it as value of hidden <input name="signature">
+    function grabCanvasImage(){
+      if(!emptyCanvas){
+        $canvasInput.val(canvas.toDataURL());
+      }
     }
-  }
 
-  //track mouse movements to allow user drawing signature
-  $canvas.mousedown(function(event){
-    emptyCanvas = false;
-    mouseOnCanvas = true;
-    draw(event.offsetX, event.offsetY, false);
-  });
-  $canvas.mousemove(function(event){
-    if(mouseOnCanvas){
-      draw(event.offsetX, event.offsetY, true);
-    }
-  });
-  $canvas.mouseup(function(){
-    mouseOnCanvas = false;
-    grabCanvasImage();
-  });
-  $canvas.mouseleave(function(){
-    mouseOnCanvas = false;
-    grabCanvasImage();
-  });
+    //track mouse movements to allow user drawing signature
+    $canvas.mousedown(function(event){
+      emptyCanvas = false;
+      mouseOnCanvas = true;
+      draw(event.offsetX, event.offsetY, false);
+    });
+    $canvas.mousemove(function(event){
+      if(mouseOnCanvas){
+        draw(event.offsetX, event.offsetY, true);
+      }
+    });
+    $canvas.mouseup(function(){
+      mouseOnCanvas = false;
+      grabCanvasImage();
+    });
+    $canvas.mouseleave(function(){
+      mouseOnCanvas = false;
+      grabCanvasImage();
+    });
+  } //end 'if(canvas)'
+
 
 }); //end $(document).ready()
