77bf4511bdcefa6f6fa33c2a05aa84ab78adb8a8
Loris <loris4526@gmail.com>
2017-08-17 07:39:55 +0200

    Remove useless directory containing explanations about project

--- explanation/signatures.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS signatures;
-
-CREATE TABLE signatures (
-  id SERIAL PRIMARY KEY,
-  first VARCHAR(255) NOT NULL,
-  last VARCHAR(255) NOT NULL,
-  signature TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
