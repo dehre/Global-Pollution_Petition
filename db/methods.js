e759baa1c94af7add4530fbb1a52c3fe9c6d6044
Loris <loris4526@gmail.com>
2017-08-18 15:17:13 +0200

    Correctly save hashed user's password into database

--- db/methods.js
+++ db/methods.js
@@ -11,23 +11,31 @@ const db = spicedPg(`postgres:${dbUser}:${dbPassword}@localhost:5432/Loris`);
 //create new user inside 'users' database
 module.exports.createUser = function(firstName,lastName,email,password){
   //hash user's password before putting into database
-
-  //set up query to put data into DB
-  const query = 'INSERT INTO users (first,last,email,password) VALUES ($1,$2,$3,$4) RETURNING id';
-  return db.query(query,[firstName,lastName,email,password]);
+  return hashPassword(password)
+  .then(function(hash){
+    //set up query to put data into DB
+    const query = 'INSERT INTO users (first,last,email,password) VALUES ($1,$2,$3,$4) RETURNING id';
+    return db.query(query,[firstName,lastName,email,hash]);
+  })
+  .catch(function(err){
+    return(`Error happened hashing password before saving into DB. Error is:\n${err}`);
+  });
 }
+
 //save new signature to DB
 module.exports.savePerson = function(firstName,lastName,signature){
   //set up query to put data into DB
   const query = 'INSERT INTO signatures (first,last,signature) VALUES ($1,$2,$3) RETURNING id';
   return db.query(query,[firstName,lastName,signature]);
 }
+
 //retrieve all people that signed the petition
 module.exports.retrievePeople = function(){
   //set up query to put data into DB
   const query = 'SELECT first,last FROM signatures';
   return db.query(query);
 }
+
 //grab user's signature for the petition
 module.exports.getSignature = function(id){
   //set up query to retrieve specific signature from DB
