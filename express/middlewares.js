fc25469cc7b7a27bdca5300cb4b06294c0ac4d0e
Loris <loris4526@gmail.com>
2017-08-22 21:15:46 +0200

    Redirect logged in users away from '/register' and '/login'

--- express/middlewares.js
+++ express/middlewares.js
@@ -3,24 +3,31 @@ const express = require('express');
 const dbMethods = require('../database/methods');
 //get back secret for hashing cookies
 const {sessionSecret} = require('../secret.json');
+
 //set all middlewares used inside express app
 module.exports.middlewares = function(app){
+
   app.use(require('body-parser').urlencoded({
       extended: false
   }));
   //use 'cookie-session' to provide data integrity for cookies
   app.use(require('cookie-session')({
     secret: sessionSecret,
+    //make session lasting 2 weeks
     maxAge: 1000*60*60*24*14
   }));
-  //redirect not registered users to GET'/register' if they're accessing private pages
+
+  //redirect not registered users to GET'/register' if they're accessing private pages; also redirect registered users to GET'/petition' if they're trying to access registration-login pages
   app.use(function(req,res,next){
-    const allowedUrls = ['/register','/login'];
-    if(allowedUrls.indexOf(req.url)===-1 && !req.session.user){
+    const publicUrls = ['/register','/login'];
+    if(publicUrls.indexOf(req.url)===-1 && !req.session.user){
       return res.redirect('/register');
+    } else if(publicUrls.indexOf(req.url)>-1 && req.session.user){
+      return res.redirect('/petition');
     }
     next();
   });
+
 };
 
 //if users try to sign petition that have signed already (so they try to GET'/petition'), redirect to '/signed' to show they're signature
