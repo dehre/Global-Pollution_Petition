e64a89e4cab58d663a58c697847cd93d65479b49
Loris <loris4526@gmail.com>
2017-08-24 18:15:20 +0200

    Refactor bonus exercise: remove one level deep in promises

--- express/rootRouter.js
+++ express/rootRouter.js
@@ -80,37 +80,37 @@ router.route('/login')
     .catch(function(err){
       console.log(`Error inside ${req.method}'${req.url}'--> ${err}`);
       //take count of bad logins
-      redisCache.get('wrongAttempt')
-      .then(function(wrongAttemptStr){
-        const wrongAttempt = parseInt(wrongAttemptStr) || 1;
-        return wrongAttempt
-      })
-      .then(function(wrongAttempt){
-        if(wrongAttempt>=3){
-          //if user tries bad login more than 3 times, give him punish time of 90sec, 180sec, 360 sec and so on
-          return redisCache.get('punishTime')
-          .then(function(timeStr){
-            return parseInt(timeStr) || 10;
+      return redisCache.get('wrongAttempt')
+    })
+    .then(function(wrongAttemptStr){
+      const wrongAttempt = parseInt(wrongAttemptStr) || 1;
+      return wrongAttempt
+    })
+    .then(function(wrongAttempt){
+      if(wrongAttempt>=3){
+        //if user tries bad login more than 3 times, give him punish time of 90sec, 180sec, 360 sec and so on
+        return redisCache.get('punishTime')
+        .then(function(timeStr){
+          return parseInt(timeStr) || 90;
+        })
+        .then(function(time){
+          req.session.punishTime = `You failed ${wrongAttempt} times. Please wait ${time} seconds before trying again`;
+          return redisCache.setex('punishTime',time,JSON.stringify(time*2))
+          .then(function(){
+            return {wrongAttempt,time}
           })
-          .then(function(time){
-            req.session.punishTime = `You failed ${wrongAttempt} times. Please wait ${time} seconds before trying again`;
-            return redisCache.setex('punishTime',time,JSON.stringify(time*2))
-            .then(function(){
-              return {wrongAttempt,time}
-            })
-          })
-        }
-        //return a default time if no onw provided
-        return {wrongAttempt:wrongAttempt,time:10};
-      })
-      .then(function(obj){
-        return redisCache.setex('wrongAttempt',obj.time,JSON.stringify(obj.wrongAttempt+1))
-      })
-      .then(function(){
-        req.session.errorMessage = 'Incorrect credentials. Please try again'
-        return res.redirect('/login');
-      });
-    }); //end 'catch' block
+        })
+      }
+      //return a default time of 10 seconds if no one provided
+      return {wrongAttempt:wrongAttempt,time:10};
+    })
+    .then(function(obj){
+      return redisCache.setex('wrongAttempt',obj.time,JSON.stringify(obj.wrongAttempt+1))
+    })
+    .then(function(){
+      req.session.errorMessage = 'Incorrect credentials. Please try again'
+      return res.redirect('/login');
+    });
   });
 
 router.get('/logout',function(req,res){
